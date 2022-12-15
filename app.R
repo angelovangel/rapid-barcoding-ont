c55fd42fe0ea867269b9332c26e367acf013736c
angelovangel <aangeloo@gmail.com>
2022-12-15 17:03:06 +0300

    app work

--- app.R
+++ app.R
@@ -14,7 +14,7 @@ barcodes <- str_c('barcode', formatC(1:96, width = 2, flag = '0'))
 make_dest <- function() {
     dest <- tibble(well = wells_colwise, 
                    sample_name = NA, 
-                   barcode = NA, dna_size = NA, conc = NA)
+                   barcode = NA, dna_size = NA, conc = NA, fmoles = NA, ul = NA)
     dest
 }
 
@@ -33,13 +33,17 @@ ui <- dashboardPage(
     fluidRow(
       box(width = 12, height = 2800, status = "info", solidHeader = FALSE, title = "Enter sample information and assign barcodes", collapsible = F,
           fluidRow(
-            column(2, numericInput('ng', 'ng per reaction', value = 100, min = 10, max = 1000, step = 10)),
+            column(2, numericInput('ng', 'ng per reaction (50-100 ng)', value = 100, min = 10, max = 500, step = 10)),
             column(2, actionButton('protocol', 'Show protocol', width = '100%', style = 'margin-top:25px')),
-            column(2, actionButton('deck', 'Show deck layout', width = '100%', style = 'margin-top:25px'))
+            column(2, actionButton('deck', 'Show deck layout', width = '100%', style = 'margin-top:25px')),
+            column(2, actionButton('download', 'Download plate layout', width = '100%', style = 'margin-top:25px'))
           ),
           
           column(4, rHandsontableOutput('hot')),
-          column(8, reactableOutput('plate'))
+          column(8, reactableOutput('plate'), 
+                 tags$hr(),
+                 tags$p("ONT rapid barcode plate"), 
+                 reactableOutput('barcode_plate'))
           )
       )
   )
@@ -49,36 +53,64 @@ ui <- dashboardPage(
 
 # server #
 server = function(input, output, session) {
-  
+    hot <- reactive({
+      if(!is.null(input$hot)) {
+      as.tibble(hot_to_r(input$hot)) %>%
+        mutate(fmoles = input$ng/((dna_size*617.96) + 36.04) * 1000000) %>%
+        mutate(ul = input$ng/conc)
+      } else {
+          example_table
+        }
+    })
+    
     output$hot <- renderRHandsontable({
-      rhandsontable(example_table,
+      rhandsontable(hot(),
                     stretchH  = 'all',
                     height = 2800,
                     rowHeaders = NULL) %>%
         hot_col('well', readOnly = T) %>%
-        hot_col('dna_size', format = '0')
+        hot_col('fmoles', readOnly = T) %>%
+        hot_col('ul', readOnly = T) %>%
+        hot_col('dna_size', format = '0') %>%
+        hot_cell(1, 3, 'test')
     })
     
     plate <- reactive({
       if(!is.null(input$hot)) {
-        df <- as.tibble(hot_to_r(input$hot)) %>% mutate(sample_name = str_c(sample_name, "<br>", barcode))
+        df <- as.tibble(hot_to_r(input$hot)) %>% 
+          mutate(sample_name = str_c(sample_name, "<br>", barcode))
+        
         plater::view_plate(
           #hot_to_r(input$hot) , 
           df,
           well_ids_column = 'well', columns_to_display = c('sample_name')
         )
       } else {
-        plater::view_plate(example_table, well_ids_column = 'well', columns_to_display = c('sample_name', 'barcode'))
+        plater::view_plate(example_table, well_ids_column = 'well', columns_to_display = c('sample_name'))
       }
     })
     
     output$plate <- renderReactable({
-      reactable(plate()$sample_name, highlight = T, wrap = F, 
+      reactable(plate()$sample_name, 
+                highlight = T, wrap = F, 
                 bordered = T, compact = T, fullWidth = T, sortable = F, 
                 defaultColDef = colDef(minWidth = 50,
                   html = TRUE, 
                   headerStyle = list(background = "#f7f7f8", fontSize = '80%'), style = list(fontSize = '80%')
                   )
+                )
+    })
+    
+    output$barcode_plate <- renderReactable({
+      bc_plate <- make_dest()
+      bc_plate$barcode <- barcodes
+      bc_plate_view <- view_plate(bc_plate, well_ids_column = 'well', columns_to_display = 'barcode')
+      reactable(bc_plate_view$barcode, wrap = F, bordered = T,
+                compact = T, fullWidth = T, sortable = F, 
+                defaultColDef = colDef(minWidth = 50,
+                                       html = TRUE, 
+                                       headerStyle = list(background = "#f7f7f8", fontSize = '80%'), style = list(fontSize = '80%')
+                                       )
                 )
     })
 }
