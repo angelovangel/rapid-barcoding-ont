cfb9c4a6798b4c062271d6be35605a773e783803
angelovangel <aangeloo@gmail.com>
2023-05-22 14:29:56 +0300

    remove mixing in template protocol

--- rapid-ont-template.py
+++ rapid-ont-template.py
@@ -89,7 +89,7 @@ def run(ctx: protocol_api.ProtocolContext):
         [ sourceplate.wells_by_name()[v] for i, v in enumerate(sourcewells1) if volume1[i] > 0],
         [ destplate.wells_by_name()[v] for i, v in enumerate(destwells1) if volume1[i] > 0], 
         new_tip = 'always',
-        mix_after = (3, 3), 
+        #mix_after = (3, 3), 
         blow_out = True, 
         blowout_location = 'destination well'
         )
